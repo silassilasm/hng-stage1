cd555e08df330caa5c294cc6c1f6e590961ace63
silassilasm <silassilasm@gmail.com>
2017-08-23 20:18:31 +0100

    Create hngintern.sql

---
+++ hngintern.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `hngintern` (
+`S/N` int(11) NOT NULL auto_increment,
+`firstname` varchar(32) NOT NULL,
+`Gender` varchar(32) NOT NULL,
+PRIMARY KEY (`id`)
+) 
+INSERT INTO `hngintern` VALUES(1, "Mark Essien", "Male");
+INSERT INTO `hngintern` VALUES(2, "Silas Silas", "Male");
+INSERT INTO `hngintern` VALUES(3, "Ogechi Okoro", "Female");
+INSERT INTO `hngintern` VALUES(4, "Joy david", "Female");
